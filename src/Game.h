d78c1245faac15b7bbe7ef9b9bf569ca3048a8e2
Ricky McAlister <ricky.mcalister@googlemail.com>
2023-02-28 22:23:57 +1100

    Set up the game loop and render a basic window

--- src/Game.h
+++ src/Game.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <ctime>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
@@ -11,7 +12,27 @@
 class Game
 {
 private:
+    sf::VideoMode videoMode;
+    sf::RenderWindow* window;
+    bool endGame;
+    sf::Event sfmlEvent;
+
+    void initWindow();
+    void initVariables();
 
 public:
+    // Constructors and destructors
+    Game();
+    ~Game();
 
+    // Accessors
+
+    // Modifiers
+
+    // Functions
+    const bool running() const;
+    void pollEvents();
+
+    void update();
+    void render();
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,19 @@
 
 int main()
 {
+    // Initialise randome seed
+    srand(static_cast<unsigned>(time(0)));
+
+    // Initialise game object
+    Game game;
+
+    // Game loop
+    while (game.running())
+    {
+        game.update();
+        game.render();
+    }
+
     // End of application
     return 0;
 }
